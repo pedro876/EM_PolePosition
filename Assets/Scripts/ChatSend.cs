6fe0222e7689f03b5e325bef3a5603deac12c7cd
thePeter876 <38359059+thePeter876@users.noreply.github.com>
2020-06-11 11:57:34 +0200

    Inicio de la sala de espera

---
+++ Assets/Scripts/ChatSend.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ChatSend : MonoBehaviour
+{
+
+    [SerializeField] UnityEvent onEnterDown;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            onEnterDown.Invoke();
+        }
+    }
+}
